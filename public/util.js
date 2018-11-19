81408c4f49614ca5a0c2a0ab5129e8efd045aed2
dibidave <dibidave@gmail.com>
2018-11-19 14:04:01 -0800

    Fixed start end time frame to use end of time unit instead of start

--- public/util.js
+++ public/util.js
@@ -14,16 +14,14 @@ Util.get_date_range_for_status = function(status) {
   let start = null;
   let end = null;
 
-  today.startOf(status.time_unit);
-
   if(status.min_time !== null) {
-    start = today.clone();
+    start = today.clone().startOf(status.time_unit);
     start.add(status.min_time, status.time_unit);
     start.toDate();
   }
 
   if(status.max_time !== null) {
-    end = today.clone();
+    end = today.clone().endOf(status.time_unit);
     end.add(status.max_time, status.time_unit);
     end.toDate();
   }
