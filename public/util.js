a363abfb76c88963ac251db9926e5a70740115e3
dibidave <dibidave@gmail.com>
2019-08-04 22:50:15 -0700

    Fixed ordering of status columns

--- public/util.js
+++ public/util.js
@@ -13,6 +13,7 @@ Util.get_date_range_for_status = function(status) {
   let start = null;
   let end = null;
   let time_unit = null;
+  let time_unit_to_add = null;
 
   if(status.time_unit === "workday") {
     time_unit = "day";
@@ -21,9 +22,17 @@ Util.get_date_range_for_status = function(status) {
     time_unit = status.time_unit;
   }
 
+  if(status.time_unit === "isoWeek") {
+    time_unit_to_add = "week";
+  }
+  else {
+    time_unit_to_add = status.time_unit;
+  }
+
   if(status.min_time !== null) {
-    start = today.clone().startOf(status.time_unit);
-    start.add(status.min_time, status.time_unit);
+
+    start = today.clone().startOf(time_unit);
+    start.add(status.min_time, time_unit_to_add);
 
     if(status.time_unit === "workday") {
       while(start.isoWeekday() > 5) {
@@ -34,22 +43,25 @@ Util.get_date_range_for_status = function(status) {
   }
 
   if(status.max_time !== null) {
-    end = today.clone().endOf(status.time_unit);
-    end.add(status.max_time, status.time_unit);
+    end = today.clone().endOf(time_unit);
+
+    end.add(status.max_time, time_unit_to_add);
 
     if(status.time_unit === "workday") {
       while(end.isoWeekday() > 5) {
         end.add(1, "day");
       }
     }
-    
+
     end.add(datastore.config.end_of_day_offset, "hours");
 
     end.toDate();
   }
 
-  return {
+  var range = {
     "start": start,
     "end": end
-  }
+  };
+
+  return range;
 };
